38302526bc5f701d9d5fb96e26e5166f43ae06a5
Sam Deane <sam@elegantchaos.com>
2026-02-26 21:22:17 +0000

    Use newer plugin

--- Package.swift
+++ Package.swift
@@ -3,34 +3,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "Octoid",
-    platforms: [
-        .macOS(.v26), .macCatalyst(.v26), .iOS(.v26), .tvOS(.v26), .watchOS(.v11)
-    ],
-    products: [
-        .library(
-            name: "Octoid",
-            targets: ["Octoid"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/elegantchaos/Logger.git", from: "2.0.1"),
-        .package(url: "https://github.com/elegantchaos/JSONSession.git", from: "2.0.0"),
-        .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "2.1.0"),
-    ],
-    targets: [
-        .target(
-            name: "Octoid",
-            dependencies: [
-                "Logger",
-                "JSONSession"
-            ]),
-        .testTarget(
-            name: "OctoidTests",
-            dependencies: ["Octoid"],
-            resources: [
-                .process("Resources")
-            ]
-        ),
-    ],
-    swiftLanguageModes: [.v5]
+  name: "Octoid",
+  platforms: [
+    .macOS(.v26), .macCatalyst(.v26), .iOS(.v26), .tvOS(.v26), .watchOS(.v11),
+  ],
+  products: [
+    .library(
+      name: "Octoid",
+      targets: ["Octoid"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/elegantchaos/Logger.git", from: "2.0.1"),
+    .package(url: "https://github.com/elegantchaos/JSONSession.git", from: "2.0.0"),
+    .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "2.1.1"),
+  ],
+  targets: [
+    .target(
+      name: "Octoid",
+      dependencies: [
+        "Logger",
+        "JSONSession",
+      ]),
+    .testTarget(
+      name: "OctoidTests",
+      dependencies: ["Octoid"],
+      resources: [
+        .process("Resources")
+      ]
+    ),
+  ],
+  swiftLanguageModes: [.v5]
 )
